1b793b5f42e7084c212c969f705b58dbbad382e8
milan-susa <milansusa@outlook.com>
2023-01-04 16:07:23 +0100

    configure igw and default rtb

--- main.tf
+++ main.tf
@@ -23,8 +23,15 @@ resource "aws_subnet" "myapp-subnet-1" {
   }
 }
 
-resource "aws_route_table" "myapp-route-table" {
+resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name: "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -32,13 +39,6 @@ resource "aws_route_table" "myapp-route-table" {
   }
 
   tags = {
-    Name: "${var.env_prefix}-rtb"
-  }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name: "${var.env_prefix}-igw"
+    Name: "${var.env_prefix}-main-rtb"
   }
 }
